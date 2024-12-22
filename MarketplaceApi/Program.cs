51a4ddd2cdd83267407c20867412e0d631974f96
Ares25061 <amilutin127@gmail.com>
2024-12-22 22:04:52 +0300

    dotnet

--- MarketplaceApi/Program.cs
+++ MarketplaceApi/Program.cs
@@ -64,7 +64,7 @@ namespace MarketplaceApi
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            
+
             builder.Services.AddSwaggerGen(options =>
             {
                 options.SwaggerDoc("v1", new OpenApiInfo
@@ -142,7 +142,7 @@ namespace MarketplaceApi
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-            
+
             app.UseCors("AllowSpecificOrigins");
 
             app.UseHttpsRedirection();
