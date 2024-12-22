d0ba76a710fb484730f09d2bebc0697c5c295568
Ares25061 <amilutin127@gmail.com>
2024-12-22 20:05:10 +0300

    testim

--- MarketplaceApi/Program.cs
+++ MarketplaceApi/Program.cs
@@ -4,7 +4,6 @@ using BusinessLogic.Services;
 using DataAccess.Wrapper;
 using Domain.Interfaces;
 using Domain.Models;
-using Mapster;
 using MarketplaceApi.Authorization;
 using MarketplaceApi.Helpers;
 using Microsoft.AspNetCore.Mvc;
@@ -19,19 +18,28 @@ namespace MarketplaceApi
         public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Настройка CORS
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowSpecificOrigins", builder =>
                 {
-                    builder.WithOrigins("https://localhost:7203", "https://oxygenmarketsite.onrender.com", "https://oxygenmarketapi.onrender.com")
-                        .AllowAnyHeader()
-                        .AllowAnyMethod()
-                        .AllowCredentials();
+                    builder.WithOrigins(
+                        "https://localhost:7203", // Ваш локальный домен
+                        "https://oxygenmarketsite.onrender.com", // Ваш сайт
+                        "https://oxygenmarketapi.onrender.com" // Ваш API
+                    )
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials(); // Разрешить отправку куки и заголовков авторизации
                 });
             });
-            builder.Services.AddDbContext<MarketpalceContext>(
-                options => options.UseSqlServer(builder.Configuration["ConnectionString"]));
 
+            // Настройка базы данных
+            builder.Services.AddDbContext<MarketpalceContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+
+            // Регистрация сервисов
             builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
             builder.Services.AddScoped<IUserService, UserService>();
             builder.Services.AddScoped<IProductService, ProductService>();
@@ -55,14 +63,12 @@ namespace MarketplaceApi
             builder.Services.AddScoped<IPaymentUserService, PaymentUserService>();
             builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
 
+            // JWT и авторизация
             builder.Services.AddScoped<IJwtUtils, JwtUtils>();
             builder.Services.AddScoped<IAccountService, AccountService>();
             builder.Services.AddScoped<IEmailService, EmailService>();
 
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddMapster();
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            // Настройка Swagger
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(options =>
             {
@@ -70,7 +76,7 @@ namespace MarketplaceApi
                 {
                     Version = "v1",
                     Title = "OxygenMarketApi",
-                    Description = "Маркетплейс Кислодрод (надеюсь не кончится)",
+                    Description = "Маркетплейс Кислород",
                     Contact = new OpenApiContact
                     {
                         Name = "Сайт",
@@ -78,51 +84,57 @@ namespace MarketplaceApi
                     },
                     License = new OpenApiLicense
                     {
-                        Name = " Бекэндер (не пишите)",
+                        Name = "Разраб (помогите)",
                         Url = new Uri("https://t.me/Ares250678")
-                    },
+                    }
                 });
+
+                // Настройка авторизации через Swagger
                 options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
-                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
-                      Enter 'Bearer' [space] and then your token in the text input below.
-                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Description = "JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token.",
                     Name = "Authorization",
                     In = ParameterLocation.Header,
                     Type = SecuritySchemeType.ApiKey,
                     Scheme = "Bearer"
                 });
-                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                  {
-                    {
-                      new OpenApiSecurityScheme
-                      {
-                        Reference = new OpenApiReference
-                          {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                          },
-                          Scheme = "oauth2",
-                          Name = "Bearer",
-                          In = ParameterLocation.Header,
 
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
                         },
                         new List<string>()
-                      }
-                    });
-                // using System.Reflection;
+                    }
+                });
+
+                // Добавление комментариев из XML-файла
                 var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
             });
 
+            // Добавление контроллеров
+            builder.Services.AddControllers();
+
             var app = builder.Build();
 
+            // Миграция базы данных
             using (var scope = app.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
-
                 var context = services.GetRequiredService<MarketpalceContext>();
                 await context.Database.MigrateAsync();
+
+                // Добавление ролей, если их нет
                 if (!context.Roles.Any())
                 {
                     context.Roles.AddRange(
@@ -130,38 +142,39 @@ namespace MarketplaceApi
                         new Role { RoleName = "User" },
                         new Role { RoleName = "Support" }
                     );
-
+                    await context.SaveChangesAsync();
                 }
-                context.SaveChanges();
             }
+
+            // Настройка HTTP-конвейера
             app.UseRouting();
 
-            app.UseCors("AllowSpecificOrigins"); // CORS должен быть первым
+            // Применение CORS
+            app.UseCors("AllowSpecificOrigins");
 
+            // Настройка авторизации и аутентификации
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-            // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            // Настройка Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
+            // Настройка HTTPS
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            // Обработка ошибок
             if (app.Environment.IsProduction())
             {
                 app.UseMiddleware<ErrorHandlerMiddleware>();
             }
+
+            // JWT Middleware
             app.UseMiddleware<JwtMiddleware>();
 
+            // Настройка маршрутов
             app.MapControllers();
 
+            // Запуск приложения
             app.Run();
         }
     }
